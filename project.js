a8121cfbc56a7171bcaa2543274031ff1c6f7de6
ZaneAmetame <145873876+ZaneAmetame@users.noreply.github.com>
2023-09-28 21:37:13 +0000

    Add files via upload

---
+++ project.js
@@ -0,0 +1,183 @@
+var q1="The most difficult thing is the decision to act, the rest is merely tenacity.-Amelia Earhart"
+
+
+var q2="How wonderful it is that nobody need wait a single moment before starting to improve the world.Anne Frank"
+
+var q3="An unexamined life is not worth living.-Socrates"
+
+var q4="Everything you've ever wanted is on the other side of fear. -George Addair"
+
+
+var q5="Dream big and dare to fail. -Zane Ametame"
+
+var q6="Courage is grace under pressure. -Ernest Hemingway"
+
+var q7= "It is still best to be honest and truthful; to make the most of what we have; to be happy with simple pleasures; and have courage when things go wrong. -Laura Ingalls Wilder"
+
+var q8= "Nothing is impossible, the word itself says, ‘I'm possible!' -Audrey Hepburn"
+
+var q9="It does not matter how slowly you go as long as you do not stop. -Zane Ametame"
+
+var q10="Don't find fault, find a remedy: anyone can complain. -Henry Ford"
+
+
+
+var q11= "A man may die, nations may rise and fall, but an idea lives on. -John F. Kennedy"
+
+var q12= "I have learned over the years that when one's mind is made up, this diminishes fear. -Rosa Parks"
+
+var q13="I didn't fail the test. I just found 100 ways to do it wrong. -Benjamin Franklin"
+
+ var q14="If you're offered a seat on a rocket ship, don't ask what seat! Just get on. -Sheryl Sandberg"
+
+ var q15="With great power comes great responsibility. -Stan Lee"
+
+ var q16="I would rather die of passion than of boredom. -Vincent van Gogh"
+
+ var q17="If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough. -Oprah Winfrey"
+
+ var q18="Dreaming, after all, is a form of planning.-Gloria Steinem"
+
+ var q19="Whatever the mind of man can conceive and believe, it can achieve.-Napoleon Hill"
+
+ var q20="First, have a definite, clear practical ideal; a goal, an objective. Second, have the necessary means to achieve your ends; wisdom, money, materials, and methods. Third, adjust all your means to that end. -Aristotle"
+
+ var q21="Twenty years from now you will be more disappointed by the things that you didn't do than by the ones you did do. So, throw off the bowlines, sail away from safe harbor, catch the trade winds in your sails. Explore, Dream, Discover. -Mark Twain"
+
+ var q23="Success usually comes to those who are too busy to be looking for it. -Henry David Thoreau"
+ var q24="If you want to make your dreams come true, the first thing you have to do is wake up. -J.M. Power"
+ var q25="If you really look closely, most overnight successes took a long time. -Steve Jobs"
+ var q26=" The secret of success is to do the common thing uncommonly well. -John D. Rockefeller Jr."
+ var q27="I find that the harder I work, the more luck I seem to have. -Thomas Jefferson"
+ var q28="The future belongs to those who believe in the beauty of their dreams. -Eleanor Roosevelt"
+ var q29="Everyone experiences failure at some point in their life — even the most successful people. Although failure can be discouraging, it's important to never give up and keep going. Let these quotes motivate you to continue working toward your goals and be the most successful version of yourself."
+ var q35="The purpose of our lives is to be happy.— Dalai Lama"
+
+var q30 ="Life is what happens when you're busy making other plans.— John Lennon"
+ 
+ var q31="Get busy living or get busy dying. — Stephen King"
+ 
+ var q32= "You only live once, but if you do it right, once is enough. — Mae West"
+ 
+ var q33= "Many of life’s failures are people who did not realize how close they were to success when they gave up.– Thomas A. Edison"
+ 
+ 
+ 
+ var q34="If you want to live a happy life, tie it to a goal, not to people or things.– Albert Einstein"
+ 
+ var q36= "Never let the fear of striking out keep you from playing the game.– Babe Ruth"
+ 
+ var q37="Money and success don’t change people; they merely amplify what is already there. — Will Smith"
+ 
+ 
+ var q38= "Your time is limited, so don’t waste it living someone else’s life. Don’t be trapped by dogma – which is living with the results of other people’s thinking. – Steve Jobs"
+
+ 
+ var q39=  "Not how long, but how well you have lived is the main thing. — Seneca"
+ 
+ 
+ var q40= "If life were predictable it would cease to be life, and be without flavor. – Eleanor Roosevelt"
+ 
+ var q41= "The whole secret of a successful life is to find out what is one’s destiny to do, and then do it.– Henry Ford"
+ 
+ var q42= "In order to write about life first you must live it.– Ernest Hemingway"
+ 
+ var q43= "The big lesson in life, baby, is never be scared of anyone or anything.– Frank Sinatra"
+ 
+ 
+ 
+ var q44= "Sing like no one’s listening, love like you’ve never been hurt, dance like nobody’s watching, and live like it’s heaven on earth. – (Attributed to various sources)"
+ 
+ var q45= "Curiosity about life in all of its aspects, I think, is still the secret of great creative people. – Leo Burnett"
+ 
+ 
+ var q46= "Life is not a problem to be solved, but a reality to be experienced– Soren Kierkegaard"
+ 
+ var q47= "The unexamined life is not worth living.— Socrates"
+ 
+ var q48= "Turn your wounds into wisdom.— Oprah Winfrey"
+ 
+ 
+ 
+ var q49= "The way I see it, if you want the rainbow, you gotta put up with the rain—Dolly Parton"
+ 
+ var q50= "Do all the good you can, for all the people you can, in all the ways you can, as long as you can. — Hillary Clinton (inspired by John Wesley quote)"
+ 
+ var q51= "Don’t settle for what life gives you; make life better and build something. — Ashton Kutcher"
+ 
+ var q52=  "Everybody wants to be famous, but nobody wants to do the work. I live by that. You grind hard so you can play hard. At the end of the day, you put all the work in, and eventually it’ll pay off. It could be in a year, it could be in 30 years. Eventually, your hard work will pay off. — Kevin Hart"
+ 
+ 
+ var q53=  "Everything negative – pressure, challenges – is all an opportunity for me to rise. — Kobe Bryant"
+ 
+ var q54= "I like criticism. It makes you strong. — LeBron James"
+ var q55= "You never really learn much from hearing yourself speak. ― George Clooney"
+ 
+ var q56= "Life imposes things on you that you can’t control, but you still have the choice of how you’re going to live through this. — Celine Dion"
+ 
+ 
+ var q57= "Life is never easy. There is work to be done and obligations to be met – obligations to truth, to justice, and to liberty. — John F. Kennedy (JFK Quotes)"
+ 
+ var q58= "Live for each second without hesitation. — Elton John"
+ 
+ var q59= "Life is like riding a bicycle. To keep your balance, you must keep moving.— Albert Einstein"
+ 
+ var q60=  "Life is really simple, but men insist on making it complicated. — Confucius"
+ 
+ var q61= "Life is a succession of lessons which must be lived to be understood. — Helen Keller"
+ 
+ var q62=  "Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work. And the only way to do great work is to love what you do. If you haven't found it yet, keep looking. Don't settle. As with all matters of the heart, you'll know when you find it — Steve Jobs"
+ 
+ var q63=  "My mama always said, life is like a box of chocolates. You never know what you're gonna get. — Forrest Gump (Forrest Gump Quotes)"
+ 
+ var q64=  "Watch your thoughts; they become words. Watch your words; they become actions. Watch your actions; they become habits. Watch your habits; they become character. Watch your character; it becomes your destiny— Lao-Tze"
+ 
+ var q65= "When we do the best we can, we never know what miracle is wrought in our life or the life of another. — Helen Keller"
+ 
+ var q66=  "The healthiest response to life is joy.— Deepak Chopra"
+ var q67=  "Life is like a coin. You can spend it any way you wish, but you only spend it once. — Lillian Dickson"
+ 
+ var q68= "The best portion of a good man's life is his little nameless, unencumbered acts of kindness and of love. — Wordsworth"
+ 
+ var q69= "In three words I can sum up everything I've learned about life: It goes on. ― Robert Frost"
+ 
+ 
+ 
+ 
+ var q70= "Life is ten percent what happens to you and ninety percent how you respond to it. — Charles Swindoll"
+ 
+  var q71=" Keep calm and carry on. — Winston Churchill"
+ 
+ 
+ 
+  var q72=  "Life is about making an impact, not making an income. — Kevin Kruse"
+ 
+  var q73= "I've missed more than 9000 shots in my career. I've lost almost 300 games. 26 times I've been trusted to take the game winning shot and missed. I've failed over and over and over again in my life. And that is why I succeed. – Michael Jordan"
+ 
+  var q74=  "Every strike brings me closer to the next home run. – Babe Ruth"
+ 
+ 
+  var q75= "The two most important days in your life are the day you are born and the day you find out why. – Mark Twain"
+ 
+  var q76= "Life shrinks or expands in proportion to one's courage. – Anais Nin"
+ 
+  var q77=  "When I was 5 years old, my mother always told me that happiness was the key to life.  When I went to school, they asked me what I wanted to be when I grew up.  I wrote down ‘happy’.  They told me I didn’t understand the assignment, and I told them they didn’t understand life. – John Lennon"
+ 
+ 
+ 
+ 
+  
+
+
+let clicked=0;
+
+
+function clicks(){
+clicked=clicked+1
+return clicked
+
+}
+
+ 
+
+ 
